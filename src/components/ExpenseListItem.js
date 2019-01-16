1fe7c2448817f861d6fbfbe49486912819c44a1f
soapluke <lukaslindgren@gmail.com>
2019-01-16 18:47:49 +0100

    formated created at

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     <div>
         <Link to={`/edit/${id}`}><h3>Description: {description}</h3></Link>
         <p>Amount: {amount}</p>
-        <p>Created At: {createdAt}</p>
+        <p>Created At: {moment(createdAt).format('MMMM Do, YYYY')}</p>
     </div>
 );
 
