4438148304587ebd2564816759e1fef9186cc9ac
soapluke <lukaslindgren@gmail.com>
2019-01-21 18:22:28 +0100

    setup google auth and private routes

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -67,7 +67,6 @@ export const startSetExpenses = () => {
                     ...childSnapshot.val()
                 });
             });
-
             dispatch(setExpenses(expenses));
         });
     };
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Header from '../components/Header';
+import { Router, Route, Switch } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+import Login from '../components/Login';
 import ExpenseDashboard from '../components/ExpenseDashboard';
 import AddExpense from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
 import NotFound from '../components/NotFound';
 import Help from '../components/Help';
+import PrivateRoute from './PrivateRoute';
+
+export const history = createHistory();
 
 const AppRouter = () => (
-    <BrowserRouter>
+    <Router history={history}>
         <div>
-            <Header/>
-            <Switch>   
-                <Route path="/" component={ExpenseDashboard} exact={true}/>
-                <Route path="/create" component={AddExpense}/>
-                <Route path="/edit/:id" component={EditExpense}/>
-                <Route path="/help" component={Help}/>
+            <Switch>
+                <Route path="/" component={Login} exact={true} />
+                <PrivateRoute path="/dashboard" component={ExpenseDashboard} />
+                <PrivateRoute path="/create" component={AddExpense} />
+                <PrivateRoute path="/edit/:id" component={EditExpense} />
+                <Route path="/help" component={Help} />
                 <Route component={NotFound}/>
             </Switch>
-        </div>  
-    </BrowserRouter>
+        </div>
+    </Router>
 );
 
 export default AppRouter;
