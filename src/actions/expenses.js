7aa5e87e42c00d6a9070a39c4a6163661bbb33fe
soapluke <lukaslindgren@gmail.com>
2019-01-21 13:58:37 +0100

    more firebase redux stuff

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -34,3 +34,25 @@ export const removeExpense = ({ id } = {}) => ({
     type: 'REMOVE_EXPENSE',
     id
 });
+
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+});
+
+export const startSetExpenses = () => {
+    return (dispatch) => {
+        return database.ref('expenses').once('value').then((snapshot) => {
+            const expenses = [];
+
+            snapshot.forEach((childSnapshot) => {
+                expenses.push({
+                    id: childSnapshot.key,
+                    ...childSnapshot.val()
+                });
+            });
+
+            dispatch(setExpenses(expenses));
+        });
+    };
+};
